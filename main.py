ba2b35d9783992d4a94db0e8748959880642fcd6
jockey520 <2251447784@qq.com>
2025-10-15 21:19:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
-letters = 0   
-digits = 0    
-spaces = 0    
-others = 0    
 
-user_input = input()
+text = input()
 
-for char in user_input:
+english_chars = 0
+digits = 0
+spaces = 0
+others = 0
+
+for char in text:
     if char.isalpha(): 
-        letters += 1
-    elif char.isdigit(): 
+        english_chars += 1
+    elif char.isdigit():  
         digits += 1
-    elif char.isspace():  
+    elif char.isspace(): 
         spaces += 1
-    else:  
+    else:
         others += 1
 
 
-print(f"英文字符：{letters}")
-print(f"数字：{digits}")
-print(f"空格：{spaces}")
-print(f"其他字符：{others}")
+print(f"英文字符: {english_chars}")
+print(f"数字: {digits}")
+print(f"空格: {spaces}")
+print(f"其他字符: {others}")
